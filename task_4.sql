74075d1b16c57bea3cb2232648034c839948698c
David Oluyele <oluyeledavid@gmail.com>
2024-10-21 00:00:15 +0100

    Add task_4.sql script

--- task_4.sql
+++ task_4.sql
@@ -1,16 +1,12 @@
--- Use the information_schema database
-USE information_schema;
-
 -- Query to get full description of the 'books' table
 SELECT 
     TABLE_NAME,
     COLUMN_NAME,
-    DATA_TYPE,
-    CHARACTER_MAXIMUM_LENGTH AS MAX_LENGTH,
+    COLUMN_TYPE,
     IS_NULLABLE,
     COLUMN_DEFAULT
 FROM 
-    COLUMNS
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
     TABLE_SCHEMA = 'alx_book_store' 
     AND TABLE_NAME = 'books';
